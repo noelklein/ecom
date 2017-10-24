9f0810f3d80b52fcc70eb033d9c2749e6ce55762
Noel Klein <noel.klein@slalom.com>
2017-10-24 12:27:49 -0500

    added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductThumbnailComponent } from './product-thumbnail/product-thumbnail.component';
+import { CategoryFilterComponent } from './category-filter/category-filter.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductThumbnailComponent,
+    CategoryFilterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
